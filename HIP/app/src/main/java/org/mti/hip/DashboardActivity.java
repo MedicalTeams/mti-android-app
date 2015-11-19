0d585d1e950607bae07f1bfea58b4ff5f910a97d
Mick McCuen <micky.mccuen@cambiahealth.com>
2015-11-19 14:13:42 -0800

    dashboard and misc layout changes

--- HIP/app/src/main/java/org/mti/hip/DashboardActivity.java
+++ HIP/app/src/main/java/org/mti/hip/DashboardActivity.java
@@ -1,8 +1,8 @@
 package org.mti.hip;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -13,22 +13,65 @@ import java.util.Date;
 
 public class DashboardActivity extends SuperActivity {
 
+    private int backPressCount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        if(getIntent().getStringExtra(EXTRA_MSG) != null) {
+
+        findViewById(R.id.bt_sign_out).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(DashboardActivity.this, MainActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+                finish();
+            }
+        });
+
+
+        if (getIntent().getStringExtra(EXTRA_MSG) != null) {
             Toast.makeText(this, getIntent().getStringExtra(EXTRA_MSG), Toast.LENGTH_SHORT).show();
         }
         findViewById(R.id.new_visit).setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Visit visit = getStorageManagerInstance().newVisit();
-                visit.setDate(new Date());
-                visit.setClinician(currentUserName);
-                visit.setFacility(facilityName);
+                visit.setVisitDate(new Date());
+                visit.setStaffMemberName(currentUserName);
+                visit.setDeviceId("MAC");
+                visit.setFacilityName(facilityName);
                 startActivity(new Intent(DashboardActivity.this, DemographicEntryActivity.class));
             }
         });
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        backPressCount = 0;
+    }
+
+    @Override
+    public void onBackPressed() {
+        backPressCount++;
+        Toast.makeText(DashboardActivity.this, "Press back again to exit", Toast.LENGTH_SHORT).show();
+        if (backPressCount == 2) {
+            super.onBackPressed();
+        }
+        new CountDownTimer(2000, 2000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                backPressCount = 0;
+            }
+        }.start();
     }
 }
