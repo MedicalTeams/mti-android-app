2741d1e96027623dc37d8246f7aac29b7058e7c4
Haris Beg (r625361) <haris.beg@cambiahealth.com>
2015-11-19 16:34:08 -0800

    Initial pass atimplementing the  NetworkConnectivityManager class

--- HIP/app/src/main/java/org/mti/hip/utils/NetworkConnectivityManager.java
+++ HIP/app/src/main/java/org/mti/hip/utils/NetworkConnectivityManager.java
@@ -1,7 +1,58 @@
 package org.mti.hip.utils;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 /**
  * Created by r624513 on 11/4/15.
+ * Updated by r625361 on 11/19/15.
  */
 public class NetworkConnectivityManager {
+
+    ConnectivityManager connectivityManager;
+    NetworkInfo activeNetwork;
+
+    /**
+     * Constructor
+     *
+     * @param context
+     */
+    public NetworkConnectivityManager(Context context) {
+        activeNetwork = connectivityManager.getActiveNetworkInfo();
+        connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+    }
+
+    /**
+     *
+     * @return true if network is available
+     */
+    public Boolean isConnected() {
+        return ((activeNetwork != null) && (activeNetwork.isConnectedOrConnecting()));
+    }
+
+    /**
+     *
+     * @return true if the devide is connected to a WiFi network
+     */
+    public Boolean isWifi() {
+        return (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI);
+    }
+
+    /**
+     *
+     * @return true if the devide is connected to a Mobile network
+     */
+    public Boolean isMobile() {
+        return (activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE);
+    }
+
+    /**
+     *
+     * @return true if the devide is connected to a WiMax network
+     */
+    public Boolean isWiMax() {
+        return (activeNetwork.getType() == ConnectivityManager.TYPE_WIMAX);
+    }
+
 }
